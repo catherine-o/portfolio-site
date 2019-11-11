40443bfcf640b3b2839b378220e1f0b158fff14b
catherine-o <catherine.ohara@outlook.com>
2019-11-11 16:17:11 -0700

    renders drawing jpgs in Drawing section

--- src/components/Drawings.vue
+++ src/components/Drawings.vue
@@ -1,7 +1,11 @@
 <template>
     <div class='drawings'>
         <h2>DRAWINGS</h2>
-        <p>Coming soon...</p>
+        <div class='drawing-grid'>
+            <img class='drawing-img' src='../assets/drawings/skulldrawing.jpg' alt='skull drawing'>
+            <img class='drawing-img' src='../assets/drawings/echeveria.jpg' alt='skull drawing'>
+            <img class='drawing-img' src='../assets/drawings/sadmandrawing.jpg' alt='skull drawing'>
+        </div>
         <img class='arrows' @click='scrollToTop' src='../assets/icons/uparrows.svg' alt='Page Up'>
     </div>
 </template>
@@ -20,12 +24,22 @@ export default {
 .drawings {
     padding: 5%;
     height: 73vh;
+    .drawing-grid {
+        display: flex;
+        align-items: center;
+        justify-content: space-evenly;
+        padding-top: 20px;
+        .drawing-img {
+            width: 300px;
+            border: 25px solid black;
+        }
+    }
     .arrows {
         width: 25px;
         opacity: 60%;
         position: relative;
         left: 46%;
-        margin-top: 35%;
+        margin-top: 2%;
         @media screen and (min-width: 1000px) {
             left: 49%;
         }
