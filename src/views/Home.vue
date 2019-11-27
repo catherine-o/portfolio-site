66de03d730cebe808f5f0f8eed0aa29ba15ca78a
catherine-o <catherine.ohara@outlook.com>
2019-11-26 20:25:36 -0700

    changes icons to links rather than a click event and opens link in new tab

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,11 +4,16 @@
             <header>
                 <h1 >CATHERINE O'HARA</h1>
                 <div class='icons'>
-                    <img src='../assets/icons/linkedinlogo.svg' @click='openLinkedIn' alt='LinkedIn'>
-                    <img src='../assets/icons/githublogo.png' @click='openGitHub' alt='GitHub'>
-                    <img src='../assets/icons/mediumlogo.svg' @click='openMedium' alt='Medium'>
-                    <img src='../assets/icons/twitterlogo.svg' @click='openTwitter' alt='Twitter'>
-                    <img src='../assets/icons/resumelogo.svg' @click='openResume' alt='Resume'>
+                    <a href='https://www.linkedin.com/in/catherine-o/' target='_blank'>
+                        <img src='../assets/icons/linkedinlogo.svg' alt='LinkedIn'></a>
+                    <a href='https://github.com/catherine-o' target='_blank'>
+                        <img src='../assets/icons/githublogo.png' @click='openGitHub' alt='GitHub'></a>
+                    <a href='https://medium.com/@oharacatherine' target='_blank'>
+                        <img src='../assets/icons/mediumlogo.svg' @click='openMedium' alt='Medium'></a>
+                    <a href='https://twitter.com/catherine__oh' target='_blank'>
+                        <img src='../assets/icons/twitterlogo.svg' @click='openTwitter' alt='Twitter'></a>
+                    <a href='https://docs.google.com/presentation/d/13KNWovJC7EkSLTLU-HEQKly9R2E4Xvr9a0UGxvnCyC8/edit?usp=sharing' target='_blank'>
+                        <img src='../assets/icons/resumelogo.svg' @click='openResume' alt='Resume'></a>
                 </div>
             </header>
             <div class='sections'>
@@ -35,23 +40,6 @@ export default {
         Writings,
         Drawings,
         Contact
-    },
-    methods: {
-        openLinkedIn() {
-            window.open('https://www.linkedin.com/in/catherine-o/', '_blank')
-        },
-        openGitHub() {
-            window.open('https://github.com/catherine-o', '_blank')
-        },
-        openMedium() {
-            window.open('https://medium.com/@oharacatherine', '_blank')
-        },
-        openTwitter() {
-            window.open('https://twitter.com/catherine__oh', '_blank')
-        },
-        openResume() {
-            window.open('https://docs.google.com/presentation/d/13KNWovJC7EkSLTLU-HEQKly9R2E4Xvr9a0UGxvnCyC8/edit?usp=sharing', '_blank')
-        }
     }
 }
 </script>
